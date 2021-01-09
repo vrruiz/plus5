8767405a4975f1cf39485d17360c7b939b76854c
Victor R. Ruiz <vrruiz@gmail.com>
2021-01-09 00:17:07 +0000

    Small fix to setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     install_requires = [
         'pygame',
-    ]
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
